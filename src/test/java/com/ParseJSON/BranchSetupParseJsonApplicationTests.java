a63eee0aefbabf0e73711626459c3c69b4cd18c9
md-imteyaz <imteyazmd143@gmail.com>
2022-10-03 15:17:38 +0530

    first commit of this project

---
+++ src/test/java/com/ParseJSON/BranchSetupParseJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParseJSON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchSetupParseJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
